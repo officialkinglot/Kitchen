a6e250ee5dc45b2256caa71eb7d7d05fdccb0eb4
King-Lot <146046831+officialkinglot@users.noreply.github.com>
2025-02-13 23:23:27 -0400

    Fix build issues and force push

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'],
+    }
+  },
+  resolve: {
+    alias: {
+      'react-router-dom': 'node_modules/react-router-dom/index.js'
+    }
+  }
+});
